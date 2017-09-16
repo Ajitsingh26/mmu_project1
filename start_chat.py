ffc96a1fb7468b38996ab5e748e896ccfc76253c
ajit singh <ajit.pal26@gmail.com>
2017-09-16 14:22:51 +0530

    list of options for the spy user

--- start_chat.py
+++ start_chat.py
@@ -1,20 +1,37 @@
+from read_message import read_message
+from send_message import send_message
+from globals import friends
+from spy_details import spy
 from add_status import add_status
-
-def start_chat(spy_name , spy_age, spy_rating):
+from add_friend import add_friend
+def start_chat(name,age,rating,status):
     current_status_message = None
-    show_menu =True
+    show_menu = True
+
     while show_menu:
-        menu_choices="what do you want to do: \n 1.Add status \n 2.close application "
-        menu_result=raw_input(menu_choices)
-        menu_result=int(menu_result)
+            menu_choices = "What do you want to do? \n" \
+                           " 1. Add a status update \n " \
+                           "2. Add a friend \n " \
+                           "3. Send a secret message \n" \
+                           " 4. Read a secret message \n" \
+                           " 5. Read Chats from a user \n " \
+                           "6. Close Application \n"
+            menu_choice = raw_input(menu_choices)
 
-        #validating use input
-        if menu_result == 1:
-            #action 1
-            add_status(current_status_message)
-        elif (menu_result == 2):
-            show_menu=False
-        else:
-            print"wrong choice enter again"
+            if len(menu_choice) > 0:
+                menu_choice = int(menu_choice)
 
+                if menu_choice == 1:
+                    current_status_message = add_status(current_status_message)
+                elif menu_choice == 2:
+                    no_of_friends = add_friend()
+                    print 'You have %d friends' % (no_of_friends)
+                elif menu_choice == 3:
+                    send_message()
+                elif (menu_choice == 4):
+                    read_message()
+                elif menu_choice == 6:
+                    show_menu=False
 
+                else:
+                    print 'Sorry you are not of the correct age to be a spy'
